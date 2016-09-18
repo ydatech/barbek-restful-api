9b456b59efe47bf584f1f31c722f027bb9840c91
Yuda  Sukmana <yudasukmana@Yudas-MacBook-Air.local>
2016-09-18 21:49:59 +0700

    JWT and User identity

--- config/params.php
+++ config/params.php
@@ -2,4 +2,8 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'JWT'=>[
+        'secret_key'=>'M2Qj5zbmDY',
+        'algorithm'=>'HS512'
+    ]
 ];
--- models/User.php
+++ models/User.php
@@ -2,103 +2,160 @@
 
 namespace app\models;
 
-class User extends \yii\base\Object implements \yii\web\IdentityInterface
+use Yii;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+use yii\behaviors\TimestampBehavior;
+use yii\web\UnauthorizedHttpException;
+use \Firebase\JWT\JWT;
+
+class User extends ActiveRecord implements IdentityInterface
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
-
+    /*
+    * Override method tableName()
+    * karena kita tidak menggunakan prefix tbl_ pada database barbek
+    * dengan nilai balikkan berupa (string) user
+    */
+    
+    public static function tableName(){
+        
+        return 'user';
+    }
+    
+    
+    /*
+    * menambahkan Timestamp Behavior
+    */
+    public function behaviors()
+    {
+        return [
+        TimestampBehavior::className(),
+        ];
+    }
+    
+    
     /**
-     * @inheritdoc
-     */
+    * @inheritdoc
+    */
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return static::findOne($id);
     }
-
+    
     /**
-     * @inheritdoc
-     */
+    *  Temukan user berdasarkan JWT token yang diberikan melalui  @param $token
+    */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
+        $secret = base64_encode(Yii::$app->params['JWT']['secret_key']);
+        try{
+            // decode token
+            
+            $jwt = JWT::decode($token,$secret, [Yii::$app->params['JWT']['algorithm']]);
+            
+            // temukan user berdasrkan jti / id user
+            
+            return static::findOne($jwt->jti);
+            
+        }catch(\Exception $e){
+            
+            // throw UnauthorizedHttpException jika token tidak valid
+            throw new UnauthorizedHttpException(Yii::t('yii','Invalid or Expired Token'));
+            
         }
-
-        return null;
+        
     }
-
+    
     /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
+    * Finds user by username
+    *
+    * @param string $username
+    * @return static|null
+    */
     public static function findByUsername($username)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return static::findOne(['username'=>$username]);
+        
     }
-
+    
     /**
-     * @inheritdoc
-     */
+    * retrun uniq id dari user (primary key)
+    * @inheritdoc
+    */
     public function getId()
     {
         return $this->id;
     }
-
+    
     /**
-     * @inheritdoc
-     */
+    * @inheritdoc
+    * Tidak akan digunakan
+    * Karena aplikasi kita tidak menggunakan autologin
+    * Aplikasi BarBek stateless
+    */
     public function getAuthKey()
     {
-        return $this->authKey;
+        return null;
     }
-
+    
     /**
-     * @inheritdoc
-     */
+    * @inheritdoc
+    * Tidak akan digunakan
+    * Karena aplikasi kita tidak menggunakan autologin
+    * Aplikasi BarBek stateless
+    */
     public function validateAuthKey($authKey)
     {
-        return $this->authKey === $authKey;
+        return false;
     }
-
+    
     /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return boolean if password provided is valid for current user
-     */
+    * Validasi password dengan password hash
+    *
+    * @param string $password password to validate
+    * @return boolean if password provided is valid for current user
+    */
     public function validatePassword($password)
     {
-        return $this->password === $password;
+        return Yii::$app->getSecurity()->validatePassword($password, $this->password);
+    }
+    
+    /*
+    * Generate password hash dari password yang diberikan user kemudian simpan pada field password
+    *
+    */
+    public function setPassword($password){
+        
+        $this->password = Yii::$app->security->generatePasswordHash($password);
+    }
+    
+    /*
+    * Generate JWT untuk user
+    *
+    */
+    
+    public function generateJWT(){
+        
+        // configurasi JWT Token
+        $secret      = base64_encode(Yii::$app->params['JWT']['secret_key']);
+        $issuedAt = time();
+        $notBefore = $issuedAt;
+        $expiredTime = $currentTime + (3600 * 24 * 30); // Expired dalam 30 hari
+        $hostInfo    = Yii::$app->request->hostInfo;
+        
+        
+        
+        $token = [
+        
+        'iat'  => $issuedAt,         // Issued at: waktu saat token digenerate
+        'jti'  => $this->getId(),       // Json Token Id: user id
+        'iss'  => $hostInfo,       // Issuer : domain dimana token digenerate/dibuat
+        'aud' => $hostInfo,    // audience : domain dimana token bisa digunakan
+        'nbf'  => $notBefore,        // Not before : token dapat digunakan sebelum
+        'exp'  => $expiredTime,           // Expired time : waktu token expired
+        ];
+        
+        
+        return JWT::encode($token, $secret, Yii::$app->params['JWT']['algorithm']);
     }
 }
